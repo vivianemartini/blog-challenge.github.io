82d00e4649f6fe11320b72a068762460bba232c7
Vivi - pessoal <martini.viviane@gmail.com>
2022-07-24 16:02:43 -0300

    add heart style and search box feature

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeHeartColor(heart){
+
+    if (heart.style.color === '#574AE8' ){
+      heart.style.color='#717171';
+    } else {
+      heart.style.color='#574AE8';
+    }
+}
+
+console.log(heart)
